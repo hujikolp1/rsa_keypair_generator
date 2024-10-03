18a468c12a448b800890dbde01f5aa0cb6f96d94
harold <haroldulrich@protonmail.com>
2024-10-03 15:49:52 -0700

    Generate D v2 extendedEuclidean

--- src/components/mainFunctions/generateD.tsx
+++ src/components/mainFunctions/generateD.tsx
@@ -25,26 +25,3 @@ export const generateD = (phiN: number, E: number): number => {
 
     return D;
 };
-
-
-
-// export const generateD = (phiN: number, E: number): number => {
-//     // d*e = 1 mod N 
-//     let D: number = 1; 
-
-//     while( ((D * Number(E)) % phiN !== 1) && D<10000 ) {
-//         if((D*Number(E)) % phiN === 1 ){
-//             break
-//         }
-//         D++;
-
-//         if(D===10000) {
-//             let errorD: string = ('Computation > 10000. Try Again.');
-//             D=-1; 
-//             console.error('ERROR in generateD functional component: ', errorD)
-//             window.location.reload(); 
-//             break; 
-//         }
-//     }
-//     return D; 
-// };
