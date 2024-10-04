ce00395a80227fffde08c8c1b8c8cb625147925c
harold <haroldulrich@protonmail.com>
2024-10-03 17:42:59 -0700

    output decryption

--- src/components/encoding/outputDecrypted.tsx
+++ src/components/encoding/outputDecrypted.tsx
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from "../../../pkgs/react";
 
-const OutputDecrypted = (props: {inputNumProp: number; decryptedNum: any; }) => {
-    const [decryptStyleText, setDecryptStyleText] = useState<Object>({}); 
 
-    useEffect( () => {
-        if( Number(props.inputNumProp) == Number(props.decryptedNum) ) {
-            setDecryptStyleText({color: 'green',textDecoration:'none',fontWeight:'bold'});
+const OutputDecrypted = (props: { inputNumProp: number; decryptedNum: number | null; }) => {
+    const [decryptStyleText, setDecryptStyleText] = useState<React.CSSProperties>({});
+
+    useEffect(() => {
+        if (props.decryptedNum !== null) {
+            // Convert both to numbers for comparison
+            const inputNum = Number(props.inputNumProp);
+            const decryptedNum = Number(props.decryptedNum);
+
+            setDecryptStyleText(
+                inputNum === decryptedNum
+                    ? { color: 'green', textDecoration: 'none', fontWeight: 'bold' }
+                    : { color: 'red', textDecoration: 'line-through', fontWeight: 'normal' }
+            );
         } else {
-            setDecryptStyleText({color: 'red', textDecoration:'line-through',fontWeight:'none'});
+            // Reset styles if decryptedNum is null
+            setDecryptStyleText({});
         }
-        return () => {}
-    },[props.decryptedNum])
+    }, [props.inputNumProp, props.decryptedNum]);
 
     return (
-        <h5 style={ decryptStyleText }>
-            Decrypted: { props.decryptedNum ? props.decryptedNum : 'N/A' } 
-        </h5> 
-    )
+        <h5 style={decryptStyleText}>
+            Decrypted: {props.decryptedNum !== null ? props.decryptedNum : 'N/A'}
+        </h5>
+    );
 };
 
-export default OutputDecrypted; 
+export default OutputDecrypted;
+
