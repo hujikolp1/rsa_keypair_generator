b86a61f1c97f0691b424efd5f2bdf2b4213ea072
harold <haroldulrich@protonmail.com>
2024-10-03 17:03:23 -0700

    dep v1

--- src/components/mainFunctions/genRandomPrimes.tsx
+++ src/components/mainFunctions/genRandomPrimes.tsx
@@ -29,16 +29,3 @@ export const genRandomPrimes = (primeArray: number[]): number[] => {
 
     return [prime1, prime2];
 };
-
-
-// export const genRandomPrimes = (primeArray: number[]): number[] => {
-//     let random1:number = Math.floor(Math.random()*(primeArray.length)) 
-//     let random2:number = Math.floor(Math.random()*(primeArray.length)) 
-//     // make sure they are unique, since a square number would not be RSA secure 
-//     if (random2 == random1){
-//         while (random2 == random1){
-//             random2 = Math.floor(Math.random() * (primeArray.length)) 
-//         }
-//     }
-//     return [primeArray[random1], primeArray[random2]]; 
-// };
