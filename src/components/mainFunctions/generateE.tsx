0bb17dc13c8d839a394657367b399274b6941e75
harold <haroldulrich@protonmail.com>
2024-10-03 17:20:30 -0700

    gen e test

--- src/components/mainFunctions/generateE.tsx
+++ src/components/mainFunctions/generateE.tsx
@@ -1,11 +1,15 @@
 import {gcd} from './gcd.tsx'; 
 
 export const generateE = (N: number, phiN: number): number => {
-        let findE: number; 
-        findE = Math.floor(Math.random()*(phiN-2)+2);
-        while(gcd(findE,N)!==1 || gcd(findE,phiN)!==1){
-            findE--;
+    let findE: number; 
+    findE = Math.floor(Math.random() * (phiN - 2) + 2);
+
+    while (gcd(findE, N) !== 1 || gcd(findE, phiN) !== 1) {
+        if (findE <= 2) {  // Safeguard against going below 2
+            throw new Error("No valid E found.");
         }
+        findE--;
+    }
 
     return findE; 
 };
