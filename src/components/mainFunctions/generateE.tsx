0ba1b5a6a87b2bfad418ece934f35014a0d23ed7
harold <haroldulrich@protonmail.com>
2024-10-03 17:25:20 -0700

    test gen e

--- src/components/mainFunctions/generateE.tsx
+++ src/components/mainFunctions/generateE.tsx
@@ -1,14 +1,17 @@
-import {gcd} from './gcd.tsx'; 
+import { gcd } from './gcd.tsx'; 
 
 export const generateE = (N: number, phiN: number): number => {
     let findE: number; 
     findE = Math.floor(Math.random() * (phiN - 2) + 2);
 
-    while (gcd(findE, N) !== 1 || gcd(findE, phiN) !== 1) {
-        if (findE <= 2) {  // Safeguard against going below 2
-            throw new Error("No valid E found.");
-        }
+    // Start looking for a valid E from the generated findE downwards
+    while (findE > 2 && (gcd(findE, N) !== 1 || gcd(findE, phiN) !== 1)) {
         findE--;
+    }
+
+    // If we exit the loop and findE is less than or equal to 2, it means no valid E was found
+    if (findE <= 2) {
+        throw new Error("No valid E found.");
     }
 
     return findE; 
